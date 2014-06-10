91d1f9f86db30451611383a3fa906a34734fba0f
Hackbright Student <info@hackbrightacademy.com>
2014-06-09 17:15:55 -0700

    Ex06: Yayyyyyyy! Finally got it alphabetized

--- ex06.py
+++ ex06.py
@@ -1,31 +1,41 @@
 from sys import argv
 
-filename= argv[1]
+filename = argv[1]
 
-input_file= open(filename)
+input_file = open(filename)
 
-rest_rate={}
+rest_rate = {}
 
 def make_dict():
     for line in input_file:
-        line= line.rstrip()
+        line = line.rstrip()
         entry = line.split(':')
 
         rest_rate[entry[0]] = entry[1]
-    return rest_rate
 
+        sorted_rest_rate_list = sorted(rest_rate)
+    
+    return sorted_rest_rate_list
 
-def create_rest_report():
-    for restaurant, score in rest_rate.iteritems():
-        print "Restaurant %s is rated at %s. \n" % (restaurant, score),
+sorted_rest_list = []
+
+def make_sorted_rest_list():
+    sorted_rest_list = sorted(rest_rate)
+    return sorted_rest_list
+
+def create_rest_report(sorted_rest_list):
+    for restaurant in sorted_rest_list:
+        print "Restaurant %s is rated at %s. \n" % (restaurant, rest_rate[restaurant]),
 
 
 def main():
 
     make_dict()
 
-    create_rest_report()
+    sorted_rest_list = make_sorted_rest_list()
+
+    create_rest_report(sorted_rest_list)
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
